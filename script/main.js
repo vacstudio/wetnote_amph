0f742ca1bc18f1299cd51e3dd979b3ecf1395a6d
Nevcan Uludas <nevcanuludas@gmail.com>
2025-11-29 18:02:34 +0300

    Add, partners section

--- script/main.js
+++ script/main.js
@@ -4,3 +4,16 @@ import { initNav } from './components/nav.js';
 document.addEventListener('DOMContentLoaded', () => {
   initNav();
 });
+
+// Load Components
+fetch("script/components/partners/partners.html")
+  .then(res => res.text())
+  .then(html => {
+    document.getElementById("Partners").innerHTML = html;
+  });
+
+  fetch("script/components/footer/footer.html")
+  .then(res => res.text())
+  .then(html => {
+    document.getElementById("Footer").innerHTML = html;
+  });
