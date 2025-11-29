356209c5fe38128b4b4e65b5d52195096f2b1239
Nevcan Uludas <nevcanuludas@gmail.com>
2025-11-29 18:59:27 +0300

    Fix, main js

--- script/main.js
+++ script/main.js
@@ -1,25 +1,25 @@
 // main.js
-import { initNav } from '../components/navbar/nav.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-  initNav();
-});
 
 // Load Components
 fetch("../components/navbar/navbar.html")
   .then(res => res.text())
   .then(html => {
     document.getElementById("Navbar").innerHTML = html;
+
+    // Run nav.js only after navbar is placed in DOM
+    import("../components/navbar/nav.js").then(module => {
+      module.initNav();
+    });
   });
 
 fetch("../components/partners/partners.html")
-  .then(res => res.text())
-  .then(html => {
-    document.getElementById("Partners").innerHTML = html;
-  });
+.then(res => res.text())
+.then(html => {
+  document.getElementById("Partners").innerHTML = html;
+});
 
 fetch("../components/footer/footer.html")
-  .then(res => res.text())
-  .then(html => {
-    document.getElementById("Footer").innerHTML = html;
-  });
+.then(res => res.text())
+.then(html => {
+  document.getElementById("Footer").innerHTML = html;
+});
